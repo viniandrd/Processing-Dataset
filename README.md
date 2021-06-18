ea71b7d9c7813b007dcd30c99c83438c90058f73
Vinicius <viniandrd@hotmail.com>
2021-06-18 13:57:34 -0300

    Extracting images

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Processing-Dataset
 Scripts to extract and process datasets (Specifically BraTS).
+
+- *input* = root path of dataset (a folder before labels HGG, LGG)
+- *output* = a path to put the extracted images. (doesn't need to exist)
+- *initial_slice* = from which slice the images should be extracted.
+- *final_slice* = until which slice the images shoud be extracted.
+
+The images are organized in the output path as follows:
+
+![](figures/structure.png)
--- extracting_nii.py
+++ extracting_nii.py
@@ -1,88 +1,104 @@
 import scipy, numpy, shutil, os, nibabel, imageio, glob
 from pathlib import Path
+from utils import printProgressBar
 
-'''input_file = 'D:/Datasets/BRATS15/train/HGG/brats_2013_pat0001_1/VSD.Brain.XX.O.MR_Flair.54512.nii'
-output_file = 'D:/Datasets/BRATS15_preprocessed/tumor/'
+def count(input, type):
+    count = 0
+    for path in Path(input).rglob('*.' + type):
+        count += 1
 
-image_array = nibabel.load(input_file).get_data()
-nx, ny, nz = image_array.shape
-print('-----')
-print('({}x{}x{}) -> {} dimensions '.format(nx, ny, nz, len(image_array.shape)))
+    return count
 
-total_slices = image_array.shape[2]
-print('Slices: ', total_slices)
-print('-----')
+def extract(input, output, initial_slice=0, final_slice=155):
+    patients_HGG = []
+    patients_LGG = []
 
-#Convert and save
+    amount = count(input, 'nii')
 
-#data = image_array[:, :, 77]
-data = numpy.rot90(numpy.rot90(numpy.rot90(image_array[:, :, 77])))
-imageio.imwrite(output_file + 'teste2.png', data)
-'''
+    c=0
+    for path in Path(input).rglob('*.nii'):
+        HGG = False
+        data = nibabel.load(path)
+        image_array = data.get_fdata()
+        nx, ny, nz = image_array.shape
+
+        print('-----')
+        print('({}x{}x{}) -> {} dimensions '.format(nx, ny, nz, len(image_array.shape)))
+        print('Slices: ', image_array.shape[2])
+        print('-----')
+
+        # Handling patient folders
+        if path.parts[-3] == 'HGG':
+            HGG = True
+            if path.parts[-2] not in patients_HGG:
+                patients_HGG.append(path.parts[-2])
+
+        else:
+            if path.parts[-2] not in patients_LGG:
+                patients_LGG.append(path.parts[-2])
+
+        if HGG:
+            output_label = output + 'HGG/'
+            index = patients_HGG.index(path.parts[-2])
+        else:
+            output_label = output + 'LGG/'
+            index = patients_LGG.index(path.parts[-2])
 
 
+        patient = 'patient{:03d}'.format(index+1)
 
+        for slice in range(initial_slice, final_slice):
+            data = numpy.rot90(numpy.rot90(numpy.rot90(image_array[:, :, slice])))
 
-output = 'D:/Datasets/BRATS15_preprocessed/tumor/'
+            if('flair' in path.parts[-1]):
+                output_tissue = output_label + 'flair/'
 
-for path in Path('D:/Datasets/BRATS15/train/HGG/').rglob('*.nii'):
-    input_file = path
-    data = nibabel.load(input_file)
-    image_array = data.get_fdata()
-    nx, ny, nz = image_array.shape
-    #print('-----')
-    #print('({}x{}x{}) -> {} dimensions '.format(nx, ny, nz, len(image_array.shape)))
-    #print('Slices: ', image_array.shape[2])
-    #print('-----')
+                if not os.path.exists(output_tissue):
+                    os.makedirs(output_tissue)
+                    print("Created ouput directory: " + output_tissue)
 
-    # Convert and save
-    # data = image_array[:, :, 77]
-    #print(path.name[18:21])
+                imageio.imwrite(output_tissue + patient + '_slice{:03d}'.format(slice) + '.png', data)
 
-    data = numpy.rot90(numpy.rot90(numpy.rot90(image_array[:, :, 91])))
-    #print(type(data))
+            if('t1' in path.parts[-1]):
+                output_tissue = output_label + 't1/'
 
-    if(path.name[18:21] == 'Fla'):
-        output_path = output + 'Flair/'
+                if not os.path.exists(output_tissue):
+                    os.makedirs(output_tissue)
+                    print("Created ouput directory: " + output_tissue)
 
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-            print("Created ouput directory: " + output_path)
+                imageio.imwrite(output_tissue + patient + '_slice{:03d}'.format(slice) + '.png', data)
 
-        imageio.imwrite(output_path + path.name[:-4] + '.png', data)
+            if('t1ce' in path.parts[-1]):
+                output_tissue = output_label + 't1c/'
 
-    if (path.name[18:21] == 'T1.'):
-        output_path = output + 'T1/'
+                if not os.path.exists(output_tissue):
+                    os.makedirs(output_tissue)
+                    print("Created ouput directory: " + output_tissue)
 
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-            print("Created ouput directory: " + output_path)
+                imageio.imwrite(output_tissue + patient + '_slice{:03d}'.format(slice) + '.png', data)
 
-        imageio.imwrite(output_path + path.name[:-4] + '.png', data)
+            if('t2' in path.parts[-1]):
+                output_tissue = output_label + 't2/'
 
-    if (path.name[18:21] == 'T1c'):
-        output_path = output + 'T1c/'
+                if not os.path.exists(output_tissue):
+                    os.makedirs(output_tissue)
+                    print("Created ouput directory: " + output_tissue)
 
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-            print("Created ouput directory: " + output_path)
+                imageio.imwrite(output_tissue + patient + '_slice{:03d}'.format(slice) + '.png', data)
 
-        imageio.imwrite(output_path + path.name[:-4] + '.png', data)
+            if('seg' in path.parts[-1]):
+                output_tissue = output_label + 'seg/'
 
-    if (path.name[18:21] == 'T2.'):
-        output_path = output + 'T2/'
+                if not os.path.exists(output_tissue):
+                    os.makedirs(output_tissue)
+                    print("Created ouput directory: " + output_tissue)
 
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-            print("Created ouput directory: " + output_path)
+                imageio.imwrite(output_tissue + patient + '_slice{:03d}'.format(slice) + '.png', data)
 
-        imageio.imwrite(output_path + path.name[:-4] + '.png', data)
+        printProgressBar(c + 1, amount, prefix='Progress:', suffix='Complete', length=50)
+        c+=1
 
-    if (path.name[18:21] == '.XX' or path.name[18:21] == '.O.'):
-        output_path = output + 'Tumor/'
-
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-            print("Created ouput directory: " + output_path)
-
-        imageio.imwrite(output_path + path.name[:-4] + '.png', data)
+if __name__ == '__main__':
+    input = 'D:/BraTS19/Data Training/'
+    output = 'D:/BraTS19/Data Training PNG/'
+    extract(input, output, 0, 155)
